f55d0952af140d70d700b2c44bb7d3eedb06e6c9
Emily Combs <emilycombswork@gmail.com>
2025-06-03 13:49:38 -0400

    Create GenerateSalesReport.py
    Outline script functions for sales report and power point

---
+++ GenerateReport.py
@@ -0,0 +1,27 @@
+
+from datetime import datetime
+
+###################
+#FIELDS
+####################
+INPUT_DIR = #path to Sales report CV
+OUTPUT_DIR = #path to outputted report
+REPORT_NAME = #Report title
+FILENAME_DATE_FORMAT = "%Y-%m-%d"
+
+###################
+#MAKE SALES REPORT METHODS
+####################
+#Make sure directory exists
+
+#Load data from CV
+
+#Create Report
+
+###################
+#CREATE POWERPOINT
+####################
+
+###################
+#MAIN
+####################
